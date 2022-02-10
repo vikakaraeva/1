a7d9afdcc58a16dfe61ca15b0d881c30c7fe950b
vikak <vikak@LAPTOP-OI65I849>
2022-02-10 10:56:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("программа сложенения двух дробных чисел:");
+            Console.WriteLine("Введите два дробных числа");
+            bool noError = true;
+            double x = 0;
+            double y = 0;
+            while (noError)
+            {
+                try
+                {
+                    x = Convert.ToDouble(Console.ReadLine());
+                    y = Convert.ToDouble(Console.ReadLine());
+                    noError = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка!!!");
+                }
+            }
+            var S = x + y;
+            Console.WriteLine("сумма="  + S);
+            Console.ReadKey();
+        }
+    }
+    
+}
